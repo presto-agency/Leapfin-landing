c821bedbbd4a22eced03ce5ab465781936de894c
Kamarhinanton <Kamarhin_anton@ukr.net>
2023-03-29 17:03:21 +0300

    feat: start

---
+++ gulp/tasks/scss.js
@@ -0,0 +1,28 @@
+import dartSass from "sass";
+import gulpSass from "gulp-sass";
+import rename from "gulp-rename";
+import cleanCss from "gulp-clean-css";
+import gulpautoprefixer from "gulp-autoprefixer";
+import autoprefixer from "autoprefixer";
+import groupCssMediaQueries from "gulp-group-css-media-queries";
+import postcss from "gulp-postcss";
+import tailwindcss from "tailwindcss";
+
+const sass = gulpSass(dartSass);
+
+export const scss = () => {
+  return app.gulp.src(app.path.src.scss, {sourcemaps: app.isDev})
+
+    .pipe(sass({outputStyle: "expanded"}))
+    .pipe(gulpautoprefixer({
+      grid: true,
+      overrideBrowserlist: ["last 3 versions"],
+      cascade: true
+    }))
+    .pipe(
+      postcss([tailwindcss('./tailwind.config.cjs'), autoprefixer()]),
+    )
+    .pipe(groupCssMediaQueries())
+    .pipe(app.gulp.dest(app.path.build.css))
+    .pipe(app.plugins.browsersync.stream())
+}
